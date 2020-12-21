2c2d72b76ed54b3ed9c36a05c5f45c2d90e3d809
Someone <rgbjkbs@hdbf.ru>
2020-12-21 20:52:49 +0200

    add tables

---
+++ database/migrations/2020_12_21_184230_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('name');
+            $table->text('description');
+            $table->char('manufacturer_id')
+                ->unsigned()->index();
+            $table->decimal('price' , 8, 2);
+            $table->char('site_manufacturers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
